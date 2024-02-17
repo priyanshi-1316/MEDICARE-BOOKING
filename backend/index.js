7ea5554c84a9c0c818ec4d7b67d52b6f6bed4c00
Priyanshi Goyal <priyanshi12.goel@gmail.com>
2024-02-17 16:41:56 +0530

    database connected

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import authRoute from "./Routes/auth.js"
+
+dotenv.config()
+
+const app=express()
+const port=process.env.PORT || 8000
+
+const corsOptions={
+    origin:true
+}
+
+app.get('/',(req,res)=>{
+    res.send("api is working");
+});
+
+//database connection
+mongoose.set('strictQuery',false)
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true,
+        })
+        console.log('MONGODB database is connected');
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+};
+
+//middleware
+app.use(express.json());
+app.use(cookieParser);
+app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute);//it is telling the app to use api for using any imcoming request that is comming
+
+app.listen(port,()=>{
+    connectDB();
+    console.log("Server is running on port "+port);
+})
